a1867211eb06011e9c086e14991a206b12537bd0
Derek Morgan <79167972+mtcderek@users.noreply.github.com>
2025-01-08 04:39:41 +0000

    refactor

---
+++ terraform-code/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  repos = {
+    infra = {
+      lang     = "terraform",
+      filename = "main.tf",
+      pages    = true
+    },
+    backend = {
+      lang     = "python",
+      filename = "main.py",
+      pages    = false
+    },
+    frontend = {
+      lang     = "javascript",
+      filename = "main.js",
+      pages    = true
+    }
+  }
+}
