485014df13fe2067872839aa74b8052877366e97
Derek Morgan <79167972+mtcderek@users.noreply.github.com>
2025-02-04 20:36:14 +0000

    complete 06-cidrsubnet

--- aws-tf/modules/infra/main.tf
+++ aws-tf/modules/infra/main.tf
@@ -36,8 +36,8 @@ resource "aws_route" "this" {
 resource "aws_subnet" "this" {
   for_each   = { for i in range(var.num_subnets): "public${i}" => i }
   vpc_id     = aws_vpc.this.id
-  cidr_block = "10.0.${each.value}.0/24"
-
+  cidr_block = cidrsubnet(aws_vpc.this.cidr_block, 8, each.value)
+  
   tags = {
     Name = "mtc-ecs-${each.key}"
   }
