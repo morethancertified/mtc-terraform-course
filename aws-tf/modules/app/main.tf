0508554fd725dc6e7af24212520fb0f55f90c54a
Derek Morgan <79167972+mtcderek@users.noreply.github.com>
2025-02-05 15:17:05 +0000

    complete 14-docker-login

--- aws-tf/modules/app/main.tf
+++ aws-tf/modules/app/main.tf
@@ -1,4 +1,22 @@
+locals {
+    ecr_url = aws_ecr_repository.this.repository_url
+    ecr_token = data.aws_ecr_authorization_token.this
+}
+
+data "aws_ecr_authorization_token" "this" {}
+
 resource "aws_ecr_repository" "this" {
   name         = var.ecr_repository_name
   force_delete = true
+}
+
+
+resource "terraform_data" "login" {
+    provisioner "local-exec" {
+        command = <<EOT
+        docker login ${local.ecr_url} \
+        --username ${local.ecr_token.user_name} \
+        --password ${local.ecr_token.password}
+        EOT
+    }
 }
