4d3e5739fa835fad37ed95674f0fbee9d1edfa6f
Derek Morgan <79167972+mtcderek@users.noreply.github.com>
2025-02-05 21:32:32 +0000

    complete 19-ecs-service-tay

--- aws-tf/modules/infra/main.tf
+++ aws-tf/modules/infra/main.tf
@@ -80,22 +80,6 @@ resource "aws_lb_listener" "this" {
   }
 }
 
-resource "aws_security_group" "alb" {
-  vpc_id = aws_vpc.this.id
-  tags = {
-    Name = "mtc-ecs-alb"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "alb" {
-  for_each          = var.allowed_ips
-  security_group_id = aws_security_group.alb.id
-  cidr_ipv4         = each.value
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
-
 resource "aws_ecs_cluster" "this" {
   name = "mtc-ecs-cluster"
 }
@@ -116,35 +100,4 @@ resource "aws_iam_role" "ecs_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-# app security group
-
-resource "aws_security_group" "app" {
-  vpc_id = aws_vpc.this.id
-  tags = {
-    Name = "mtc-ecs-app"
-  }
-}
-
-# ingress rule <- alb security group
-
-resource "aws_vpc_security_group_ingress_rule" "app" {
-  security_group_id            = aws_security_group.app.id
-  referenced_security_group_id = aws_security_group.alb.id
-  ip_protocol                  = "-1"
-  tags = {
-    Name = "allow-all-from-alb"
-  }
-}
-
-# egress rule -> world
-
-resource "aws_vpc_security_group_egress_rule" "app" {
-  security_group_id = aws_security_group.app.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-  tags = {
-    Name = "allow-all"
-  }
 }
