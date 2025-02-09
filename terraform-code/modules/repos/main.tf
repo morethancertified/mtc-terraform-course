5a897adc46ae63130bf89429b337ac58070b2bb8
Derek Morgan <79167972+mtcderek@users.noreply.github.com>
2025-02-09 21:41:54 +0000

    updated modules source

--- terraform-code/modules/repos/main.tf
+++ terraform-code/modules/repos/main.tf
@@ -1,5 +1,5 @@
 module "repos" {
-  source           = "./modules/dev-repos"
+  source           = "./dev-repos"
   for_each         = var.environments
   repo_max         = 9
   env              = each.key
@@ -9,6 +9,6 @@ module "repos" {
 
 module "deploy-key" {
   for_each  = var.deploy_key ? toset(flatten([for k, v in module.repos : keys(v.clone-urls) if k == "dev"])) : []
-  source    = "./modules/deploy-key"
+  source    = "./deploy-key"
   repo_name = each.key
 }
