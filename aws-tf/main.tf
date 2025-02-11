0a7c96dd352a0f266c7b82dfefa63b43dc7cc32d
Derek Morgan <79167972+mtcderek@users.noreply.github.com>
2025-02-11 18:25:41 +0000

    complete 23-app-module-for_each

--- aws-tf/main.tf
+++ aws-tf/main.tf
@@ -1,5 +1,28 @@
 # Root Main.tf
 
+locals {
+  apps = {
+    ui = {
+      ecr_repository_name = "ui"
+      app_path            = "ui"
+      image_version       = "1.0.1"
+      app_name            = "ui"
+      port                = 80
+      is_public           = true
+      path_pattern        = "/*"
+    }
+    api = {
+      ecr_repository_name = "api"
+      app_path            = "api"
+      image_version       = "1.0.1"
+      app_name            = "api"
+      port                = 80
+      is_public           = true
+      path_pattern        = "/*"
+    }
+  }
+}
+
 module "infra" {
   source      = "./modules/infra"
   vpc_cidr    = "10.0.0.0/16"
@@ -8,18 +31,19 @@ module "infra" {
 }
 
 module "app" {
-  source                = "./modules/app"
-  ecr_repository_name   = "ui"
-  app_path              = "ui"
-  image_version         = "1.0.1"
-  app_name              = "ui"
-  port                  = 80
+  source              = "./modules/app"
+  for_each            = local.apps
+  ecr_repository_name = each.value.ecr_repository_name
+  app_path            = each.value.app_path
+  image_version       = each.value.image_version
+  app_name            = each.value.app_name
+  port                = each.value.port
+  is_public           = each.value.is_public
+  path_pattern        = each.value.path_pattern
   execution_role_arn    = module.infra.execution_role_arn
   app_security_group_id = module.infra.app_security_group_id
   subnets               = module.infra.public_subnets
   cluster_arn           = module.infra.cluster_arn
-  is_public             = true
   vpc_id                = module.infra.vpc_id
-  path_pattern          = "/*"
   alb_listener_arn      = module.infra.alb_listener_arn
 }
